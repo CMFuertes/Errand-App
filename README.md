c0667300ae7bda39471666d6df116039455c4aa3
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2022-06-30 05:28:50 -0400

    changing js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Errand-App
+
+Username is saving in local storage, however items for the todo list are not. Will attempt to fix at a later time. 
--- main.js
+++ main.js
@@ -12,107 +12,107 @@ window.addEventListener('load', () =>  {
     })
 
     newTodoForm.addEventListener('submit', e => {
-        e.preventDefault();
+		e.preventDefault();
 
-        const todo = {
-            content: e.target.elements.content.value,
-            category: e.target.elements.category.value,
-            done: false,
-            createdAt: new Date ().getTime()
-    }
-    todos.push(todo);
-    
-    localStorage.setItem('todos', JSON.stringify(todos));
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
 
-    e.target.reset();
+		todos.push(todo);
 
-    DisplayTodos();
-    })
+		localStorage.setItem('todos', JSON.stringify(todos));
 
-    DisplayTodos()
-}) 
+		// Reset the form
+		e.target.reset();
+
+		DisplayTodos()
+	})
+
+	DisplayTodos()
+})
 
 function DisplayTodos () {
-    const todoList = document.querySelector('#todo-list');
-    todoList.innerHTML = "";
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = "";
 
-    todos.forEach (todo =>  {
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-item')
+	todos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
 
-        const label = document.createElement('label');
-        const input = document.createElement('input');
-        const span = document.createElement('span');
-        const content = document.createElement('div');
-        const actions = document.createElement('div');
-        const edit = document.createElement('button');
-        const deleteButton = document.createElement('button');
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
 
-        input.type = 'checkbox';
-        input.checked = todo.done;
-        span.classList.add('bubble');
-        if (todo.category == 'personal') {
-            span.classList.add('personal');
-        } else {
-            span.classList.add('business');
-        }
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
 
-        content.classList.add('todo-content');
-        actions.classList.add('actions');
-        edit.classList.add('edit');
-        deleteButton.classList.add('delete');
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
 
-        content.innerHTML = `<input type="text" value="${todo.conent}"
-        readonly>`;
-        edit.innerHTML = 'Edit';
-        deleteButton.innerHTML = 'Delete';
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
 
-        label.appendChild(input);
-        label.appendChild(span);
-        actions.appendChild(edit);
-        actions.appendChild(deleteButton);
-        todoItem.appendChild(label);
-        todoItem.appendChild(content);
-        todoItem.appendChild(actions);
+		todoList.appendChild(todoItem);
 
-        todoList.appendChild(todoItem);
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
 
-        if (todo.done) {
-            todoItem.classList.add('done');
-        }
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
 
-        input.addEventListener('change', (e) => {
-            todo.done = e.target.checked;
-            localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
 
-            if (todo.done) {
-                todoItem.classList.add('done');
-            } else {
-                todoItem.classList.remove('done');
-            }
+		})
 
-            DisplayTodos();
-        })
-         
-        edit.addEventListener('click', (e) => {
-            const input = content.querySelector('input');
-            input.removeAttribute('readonly');
-            input.focus();
-            input.addEventListener('blur', (e) => {
-                input.setAttribute('readonly', true);
-                todo.conent = e.target.value;
-                localStorage.setItem('todos', JSON.stringify(todos));
-                DisplayTodos();
-            })
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
 
-        })
+			})
+		})
 
-        deleteButton.addEventListener('click', (e) => {
-            todos = todos.filter(t => t != todo);
-            localStorage.setItem('todos', JSON.stringify(todos));
-            DisplayTodos();
-        })
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
 
-    })
-
- }
+	})
+}
